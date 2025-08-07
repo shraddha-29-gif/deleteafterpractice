d7a703f1a856e4960c425fabb80985bddf930e3b
shraddha-29-gif <shraddhapetkar.02@gmail.com>
2025-08-07 20:39:32 +0530

    Update declarative.groovy

--- jenkinsb25/declarative.groovy
+++ jenkinsb25/declarative.groovy
@@ -4,13 +4,15 @@ pipeline {
         stage('pull') {
             steps {
                 echo "we are pulling the source code from git repository"
-                git 'https://github.com/AnupDudhe/cloudblitz-student-app.git'
+                git branch: 'main', changelog: false, poll: false, url: 'https://github.com/shraddha-29-gif/deleteafterpractice.git'
                 //git 'https://github.com/AnupDudhe/studentapp-ui.git'
             }
         }
         stage('Build') {
             steps {
                 echo "Building this app using maven"
+                sh '''cd backend
+                mvn clean package-Dmaven.test.skip=true'''
             }
         }
         stage('Test') {
